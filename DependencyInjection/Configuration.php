a4762a3a8308a47a1d4d6c0bc0aed1c419905896
Robert <robert@mediamonks.com>
2016-02-09 13:12:01 +0100

    - Cleaned up Configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -3,6 +3,7 @@
 namespace MediaMonks\RestApiBundle\DependencyInjection;
 
 use MediaMonks\RestApiBundle\Request\Format;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -18,10 +19,21 @@ final class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('mediamonks_rest_api');
 
-        $rootNode
-            ->children()
-                ->arrayNode('request_matcher')
-                    ->addDefaultsIfNotSet()
+        $this->addRequestMatcherNode($rootNode);
+        $this->addOutputFormatNode($rootNode);
+        $this->addPostMessageOriginNode($rootNode);
+
+        return $treeBuilder;
+    }
+
+    /**
+     * @param NodeDefinition $node
+     */
+    protected function addRequestMatcherNode(NodeDefinition $node)
+    {
+        $node->children()
+            ->arrayNode('request_matcher')
+                ->addDefaultsIfNotSet()
                     ->children()
                         ->arrayNode('whitelist')
                             ->defaultValue(['~^/api/$~', '~^/api~'])
@@ -31,24 +43,37 @@ final class Configuration implements ConfigurationInterface
                         ->arrayNode('blacklist')
                             ->defaultValue(['~^/api/doc~'])
                             ->prototype('scalar')
-                            ->end()
                         ->end()
                     ->end()
-                ->end()
-                ->arrayNode('output_formats')
-                    ->defaultValue([Format::getDefault()])
-                    ->prototype('scalar')
-                    ->validate()
-                        ->ifNotInArray(Format::getAvailable())
-                        ->thenInvalid('Formats can only contain "' . implode('"', Format::getAvailable()) . '", not "%s"')
-                        ->end()
-                    ->end()
-                ->end()
-                ->scalarNode('post_message_origin')
-                    ->defaultNull()
                 ->end()
             ->end();
+    }
 
-        return $treeBuilder;
+    /**
+     * @param NodeDefinition $node
+     */
+    protected function addOutputFormatNode(NodeDefinition $node)
+    {
+        $node->children()
+            ->arrayNode('output_formats')
+                ->defaultValue([Format::getDefault()])
+                ->prototype('scalar')
+                ->validate()
+                    ->ifNotInArray(Format::getAvailable())
+                    ->thenInvalid('Formats can only contain "' . implode('"', Format::getAvailable()) . '", not "%s"')
+                    ->end()
+                ->end()
+            ->end();
+    }
+
+    /**
+     * @param NodeDefinition $node
+     */
+    protected function addPostMessageOriginNode(NodeDefinition $node)
+    {
+        $node->children()
+            ->scalarNode('post_message_origin')
+            ->defaultNull()
+            ->end();
     }
 }

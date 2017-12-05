44e0fa431a2fe611fdf4e9f3f1f4a7ea5ca448a9
Robert <robertslootjes@gmail.com>
2017-12-05 14:23:41 +0100

    Disable cache in travis

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -7,14 +7,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $path = __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Yaml/Parser.php';
 
-if (function_exists('opcache_is_script_cached')) {
-    var_dump(opcache_is_script_cached ($path));
-    var_dump(opcache_invalidate($path, true));
-    var_dump(opcache_is_script_cached ($path));
-    var_dump(opcache_compile_file($path));
-    var_dump(opcache_reset());
-}
-
 echo 'yaml parser:'.PHP_EOL;
 $yamlParser = new Parser();
 print_r(get_class_methods($yamlParser));

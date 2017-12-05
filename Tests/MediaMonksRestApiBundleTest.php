e619ccab82d74365cba8baed73751361c6a43151
Robert <robertslootjes@gmail.com>
2017-12-05 13:43:30 +0100

    Debug

--- Tests/MediaMonksRestApiBundleTest.php
+++ Tests/MediaMonksRestApiBundleTest.php
@@ -3,6 +3,8 @@
 namespace MediaMonks\RestApiBundle\Tests;
 
 use MediaMonks\RestApiBundle\MediaMonksRestApiBundle;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Yaml\Parser;
 
 class MediaMonksRestApiBundleTest extends \PHPUnit_Framework_TestCase
 {
@@ -13,5 +15,18 @@ class MediaMonksRestApiBundleTest extends \PHPUnit_Framework_TestCase
             '\MediaMonks\RestApiBundle\DependencyInjection\MediaMonksRestApiExtension',
             $bundle->getContainerExtension()
         );
+    }
+
+    public function testYamlParser()
+    {
+        echo 'yaml parser:'.PHP_EOL;
+        $yamlParser = new Parser();
+        print_r(get_class_methods($yamlParser));
+
+        echo 'version: '.Kernel::VERSION.PHP_EOL;
+
+        echo file_get_contents(__DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Yaml/Parser.php');
+
+        $this->assertTrue(true);
     }
 }

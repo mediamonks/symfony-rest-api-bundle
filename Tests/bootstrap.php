429ce0dbf948b86c4870d236d246731e71a923bf
Robert <robertslootjes@gmail.com>
2018-01-02 10:57:25 +0100

    Allow lower version of di test

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -5,7 +5,7 @@ use Symfony\Component\HttpKernel\Kernel;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$path = __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Yaml/Parser.php';
+/*$path = __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Yaml/Parser.php';
 
 echo 'yaml parser:'.PHP_EOL;
 $yamlParser = new Parser();
@@ -13,4 +13,4 @@ print_r(get_class_methods($yamlParser));
 
 echo 'version: '.Kernel::VERSION.PHP_EOL;
 
-echo file_get_contents($path);
+echo file_get_contents($path);*/
